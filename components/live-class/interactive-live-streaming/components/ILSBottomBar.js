92b618920ae01f70a58bd56354763dc3b32c864b
ApranavC <apranavc2211@gmail.com>
2026-03-21 22:41:59 +0530

    fix(ILSBottomBar): add unique key prop for map iterator and resolve React warning

--- components/live-class/interactive-live-streaming/components/ILSBottomBar.js
+++ components/live-class/interactive-live-streaming/components/ILSBottomBar.js
@@ -823,7 +823,7 @@ export function ILSBottomBar({
                 <Dialog.Panel className="w-screen transform overflow-hidden bg-gray-800 shadow-xl transition-all">
                   <div className="grid container bg-gray-800 py-6">
                     <div className="grid grid-cols-12 gap-2">
-                      {otherFeatures.map(({ icon, index }) => {
+                      {otherFeatures.map(({ icon }, index) => {
                         return (
                           <div
                             key={index}
